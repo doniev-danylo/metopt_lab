d5b966578bfed67dea421a9c992a4456f97e9221
Doniev_Danylo <danil.donev@appflame.com>
2022-02-20 20:30:45 +0200

    rename file

--- lab1.py
+++ lab1.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press ⌥⇧R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌃' to toggle the breakpoint.
+def F (x, y):
+    return 12 * x ** 2 + 14 * y ** 2 + 0.01 * x * y + 3 * x
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def Grad (x, y):
+    return np.array([24 * x + 0.01 * y + 3,  28 * y + 0.01 * x])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# сам метод
+
+def Gr_m (x1, x2):
+    alpha = 0.1  # Шаг сходимости
+    eps = 0.000001  # точность
+    X_prev = np.array([x1, x2])
+    X = X_prev - alpha * Grad(X_prev[0], X_prev[1])
+
+    while np.linalg.norm(X - X_prev) > eps :
+        X_prev = X.copy()
+        X = X_prev - alpha * Grad(X_prev[0], X_prev[1])
+        print(X_prev, X)
+
+    return X
+
+
+result = Gr_m(0.3, 0.3)
+
+print(result)
